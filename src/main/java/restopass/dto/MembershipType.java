abee321f6c55b41722046d07bd1c59cfa7314c32
Yamila Casarini <yamila.casarini@despegar.com>
2020-07-03 20:06:40 -0300

    fix name membership dish

--- src/main/java/restopass/dto/MembershipType.java
+++ src/main/java/restopass/dto/MembershipType.java
@@ -1,12 +1,16 @@
 package restopass.dto;
 
 public enum MembershipType {
+
     BASIC("Básica"),GOLD("Oro"),PLATINUM("Platinum");
 
+    private String name;
+
     MembershipType(String name) {
+        this.name = name;
     }
 
     public String getName(){
-        return this.name();
+        return name;
     }
 }
